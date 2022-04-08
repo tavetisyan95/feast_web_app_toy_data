8d6c330d12ec4b00a94cc7bb20fcedbdee7580dd
Tigran Avetisyan <tigran.1995.11@gmail.com>
2022-04-08 15:44:34 +0400

    Changing the wording in the comments

--- driver_stats/definitions.py
+++ driver_stats/definitions.py
@@ -13,14 +13,14 @@ driver = Entity(
     description="The ID of the driver"
     )
 
-# Declaring the source for raw feature data
+# Declaring the source for the first feature file
 file_source1 = FileSource(
     path=dir + r"\data\driver_stats_1.parquet",
     event_timestamp_column="event_timestamp",
     created_timestamp_column="created"
 )
 
-# Defining the features in a feature view
+# Defining the features for the first feature view
 driver_stats_fv1 = FeatureView(
     name="driver_stats_fv1",
     ttl=Duration(seconds=86400 * 2),
@@ -32,14 +32,14 @@ driver_stats_fv1 = FeatureView(
 )
 
 
-# Declaring the source for raw feature data
+# Declaring the source for the second feature file
 file_source2 = FileSource(
     path=dir + r"\data\driver_stats_2.parquet",
     event_timestamp_column="event_timestamp",
     created_timestamp_column="created"
 )
 
-# Defining the features in a feature view
+# Defining the features for the second feature view
 driver_stats_fv2 = FeatureView(
     name="driver_stats_fv2",
     ttl=Duration(seconds=86400 * 2),
